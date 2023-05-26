8b6c223005a348f1a3ede2793d76eac5f22b027a
KrishMehta <kvm3jp@virginia.edu>
2023-05-26 17:53:57 +0800

    Initial Commit

---
+++ YellowRed/YellowRedApp.swift
@@ -0,0 +1,17 @@
+//
+//  YellowRedApp.swift
+//  YellowRed
+//
+//  Created by Krish Mehta on 26/5/23.
+//
+
+import SwiftUI
+
+@main
+struct YellowRedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

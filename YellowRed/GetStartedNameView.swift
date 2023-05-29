f903092eebcf2cf17e80f71d11694cde1164f7b6
KrishMehta <kvm3jp@virginia.edu>
2023-05-29 21:35:01 +0800

    Split get started View into full name input View and phone number input View; pending full name validation algorithm

---
+++ YellowRed/GetStartedNameView.swift
@@ -0,0 +1,74 @@
+//
+//  GetStartedView.swift
+//  YellowRed
+//
+//  Created by Krish Mehta on 26/5/23.
+//
+
+import SwiftUI
+
+struct GetStartedNameView: View {
+    @State private var fullName: String = ""
+    @State private var proceed: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [.yellow, .red]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Text("Get Started")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .padding(.bottom, 50)
+                    
+                    Text("Enter Full Name")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.black)
+                    
+                    TextField("John Smith", text: $fullName)
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .foregroundColor(.black)
+                        .padding()
+                        .frame(width: 300)
+                        .background(.white)
+                        .cornerRadius(10)
+                        .padding(.bottom, 25)
+                    
+                    Button(action: {
+                        proceed = true
+                    }) {
+                        Text("Proceed")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .frame(width: 300)
+                            .background(.yellow)
+                            .cornerRadius(10)
+                            .padding(.bottom, 25)
+                    }
+                    .fullScreenCover(isPresented: $proceed) {
+                        GetStartedNumberView()
+                    }
+                }
+                .padding()
+                .cornerRadius(20)
+            }
+        }
+    }
+}
+
+struct GetStartedNameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GetStartedNameView()
+    }
+}
---
+++ YellowRed/GetStartedNumberView.swift
@@ -0,0 +1,28 @@
+//
+//  GetStartedNumberView.swift
+//  YellowRed
+//
+//  Created by Krish Mehta on 29/5/23.
+//
+
+import SwiftUI
+
+struct GetStartedNumberView: View {
+    var body: some View {
+           ZStack {
+               LinearGradient(
+                   gradient: Gradient(colors: [Color.yellow, Color.red]),
+                   startPoint: .topLeading,
+                   endPoint: .bottomTrailing
+               )
+               .edgesIgnoringSafeArea(.all)
+
+           }
+       }
+}
+
+struct GetStartedNumberView_Previews: PreviewProvider {
+    static var previews: some View {
+        GetStartedNumberView()
+    }
+}

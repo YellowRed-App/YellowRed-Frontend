85b421bcc2ab464e2dcae1f8b95ed2b29663a59d
KrishMehta <kvm3jp@virginia.edu>
2023-06-05 22:10:38 +0800

    Affiliation view with basic validation algorithm

---
+++ YellowRed/NotificationsView.swift
@@ -0,0 +1,32 @@
+//
+//  NotificationsView.swift
+//  YellowRed
+//
+//  Created by Krish Mehta on 5/6/23.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct NotificationsView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color.yellow, Color.red]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .edgesIgnoringSafeArea(.all)
+            
+        }
+    }
+}
+
+struct NotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}

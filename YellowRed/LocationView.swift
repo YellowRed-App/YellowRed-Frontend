440829ebda3b4e7ea536b357a9ce379c1a9c26ed
KrishMehta <kvm3jp@virginia.edu>
2023-06-18 19:56:18 +0800

    Location view to request user location sharing permissions; add Info.plist file with additional key-value pairs for privacy protection

--- YellowRed/LocationView.swift
+++ YellowRed/LocationView.swift
@@ -6,24 +6,118 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct LocationView: View {
-    @Environment(\.presentationMode) var presentationMode
+    @State private var next = false
     
     var body: some View {
-        ZStack {
-            LinearGradient(
-                gradient: Gradient(colors: [Color.yellow, Color.red]),
-                startPoint: .topLeading,
-                endPoint: .bottomTrailing
+        NavigationView {
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [.yellow, .red]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Spacer()
+                    
+                    Image(systemName: "location.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 128, height: 128)
+                        .padding()
+                    
+                    Text("Enable Location Services")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    Text("YellowRed will keep you posted with location-based services.")
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    Text("In order to receive alerts based on your geographic location, you must have your location sharing permissions set to \"Always\" with \"Precise Location\" enabled.")
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Button(action: {
+                            handleYesButtonTap()
+                        }) {
+                            Text("Yes")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.white)
+                                .padding(10)
+                                .frame(width: 100)
+                                .background(.yellow)
+                                .cornerRadius(10)
+                        }
+                        .padding(.top)
+                        
+                        Button(action: {
+                            handleNoButtonTap()
+                        }) {
+                            Text("No")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                                .padding(10)
+                                .frame(width: 100)
+                                .background(.white)
+                                .cornerRadius(10)
+                        }
+                        .padding(.bottom)
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.white)
+                    .cornerRadius(20)
+                }
+                .ignoresSafeArea()
+            }
+            .background(
+                NavigationLink(
+                    destination: WelcomeView(),
+                    isActive: $next,
+                    label: { EmptyView() }
+                )
             )
-            .edgesIgnoringSafeArea(.all)
-            
         }
+    }
+    
+    private func handleYesButtonTap() {
+        // TODO: fix this shit
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            DispatchQueue.main.async {
+                next = true
+            }
+        } else {
+            let locationManager = CLLocationManager()
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+    
+    private func handleNoButtonTap() {
+        next = true
     }
 }
 
-struct LocationView_Preview: PreviewProvider {
+struct LocationView_Previews: PreviewProvider {
     static var previews: some View {
         LocationView()
     }
---
+++ YellowRed/WelcomeView.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeView.swift
+//  YellowRed
+//
+//  Created by Krish Mehta on 17/6/23.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    @Environment(\.presentationMode) var presentationMode
+       
+       var body: some View {
+           ZStack {
+               LinearGradient(
+                   gradient: Gradient(colors: [Color.yellow, Color.red]),
+                   startPoint: .topLeading,
+                   endPoint: .bottomTrailing
+               )
+               .edgesIgnoringSafeArea(.all)
+               
+           }
+       }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}

3e923246be3bbc25c4edb14426b05a3e3535a871
KrishMehta <kvm3jp@virginia.edu>
2023-06-23 16:26:09 +0800

    Revert "Add right circle arrow icon to next button"
    This reverts commit 8ff56b020a17e97646a3cc1c0c37360bf4cc0c70.

--- YellowRed/GetStartedNameView.swift
+++ YellowRed/GetStartedNameView.swift
@@ -23,57 +23,53 @@ struct GetStartedNameView: View {
                 )
                 .edgesIgnoringSafeArea(.all)
                 
-                VStack(spacing: 20) {
+                VStack {
                     Text("Get Started")
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.white)
-                        .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .padding(.bottom, 50)
                     
                     Text("Enter Full Name")
                         .font(.title2)
                         .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
+                        .foregroundColor(.black)
                     
                     TextField("John Smith", text: $fullName)
                         .font(.title3)
+                        .fontWeight(.medium)
+                        .foregroundColor(.black)
+                        .autocapitalization(.words)
                         .padding()
-                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
+                        .frame(width: 300)
+                        .background(.white)
+                        .cornerRadius(10)
                         .overlay(
                             RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.black, lineWidth: isFullNameValid ? 0 : 1)
+                                .stroke(.black, lineWidth: isFullNameValid ? 0 : 1)
                         )
-                        .padding(.horizontal, 20)
                     
                     if !isFullNameValid {
                         Text("Please enter a valid name!")
                             .font(.subheadline)
                             .foregroundColor(.white)
-                            .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
                     }
                     
                     Button(action: {
                         isFullNameValid = validateFullName(fullName)
                         next = isFullNameValid
                     }) {
-                        HStack {
-                            Text("Next")
-                                .font(.title)
-                                .fontWeight(.semibold)
-                                .foregroundColor(.black)
-                            Image(systemName: "arrow.right.circle.fill")
-                                .font(.title)
-                                .foregroundColor(.black)
-                        }
-                        .padding(12.5)
-                        .frame(maxWidth: .infinity)
-                        .background(.white)
-                        .cornerRadius(15)
-                        .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 10)
+                        Text("Next")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .frame(width: 100)
+                            .background(.yellow)
+                            .cornerRadius(10)
+                            .padding(.bottom, 25)
                     }
-                    .padding(.horizontal, 20)
-                    .padding(.bottom, 25)
+                    .padding(.vertical, 25)
                     .background(
                         NavigationLink(
                             destination: GetStartedNumberView(fullName: fullName),
@@ -85,7 +81,8 @@ struct GetStartedNameView: View {
                         .hidden()
                     )
                 }
-                .padding(.horizontal)
+                .padding()
+                .cornerRadius(20)
             }
         }
     }
@@ -95,6 +92,7 @@ struct GetStartedNameView: View {
         let namePredicate = NSPredicate(format: "SELF MATCHES %@", nameRegex)
         return namePredicate.evaluate(with: name)
     }
+    
 }
 
 struct GetStartedNameView_Previews: PreviewProvider {
@@ -102,4 +100,3 @@ struct GetStartedNameView_Previews: PreviewProvider {
         GetStartedNameView()
     }
 }
-

0b83d7ec3cdc2a3056493d7698448bcbe04e8804
KrishMehta <kvm3jp@virginia.edu>
2023-06-25 22:14:00 +0800

    Implement YellowMessageView for user to select a message template or create a custom message for the Yellow Button

--- YellowRed/YellowMessageView.swift
+++ YellowRed/YellowMessageView.swift
@@ -2,7 +2,7 @@
 //  YellowMessageView.swift
 //  YellowRed
 //
-//  Created by Krish Mehta on 22/6/23.
+//  Created by Krish Mehta on 25/6/23.
 //
 
 import SwiftUI
@@ -10,15 +10,130 @@ import SwiftUI
 struct YellowMessageView: View {
     @Environment(\.presentationMode) var presentationMode
     
+    @State private var selectedTemplate: Int?
+    @State private var customMessage: String = ""
+    
+    @State private var next: Bool = false
+    
+    let messageTemplates = [
+        "Template #1",
+        "Template #2",
+        "Template #3",
+    ]
+    
     var body: some View {
         ZStack {
             LinearGradient(
-                gradient: Gradient(colors: [Color.yellow, Color.red]),
+                gradient: Gradient(colors: [.yellow, .red]),
                 startPoint: .topLeading,
                 endPoint: .bottomTrailing
             )
             .edgesIgnoringSafeArea(.all)
             
+            VStack(spacing: 20) {
+                Spacer()
+                
+                Image(systemName: "message.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 128, height: 128)
+                    .foregroundColor(.white)
+                    .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
+                
+                Text("Yellow Message")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                    .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
+                
+                Text("Please choose a message template for the Yellow Button or create your own custom message!")
+                    .font(.title3)
+                    .fontWeight(.medium)
+                    .foregroundColor(.white)
+                    .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                
+                VStack(spacing: 15) {
+                    ForEach(0..<messageTemplates.count, id: \.self) { index in
+                        Button(action: { selectedTemplate = index }) {
+                            Text(messageTemplates[index])
+                                .foregroundColor(.black)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding()
+                                .background(.white)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(selectedTemplate == index ? .black : .clear, lineWidth: 2)
+                                )
+                                .cornerRadius(10)
+                        }
+
+                    }
+                    
+                    ZStack(alignment: .leading) {
+                        if customMessage.isEmpty {
+                            Text("Custom Yellow Button Message")
+                                .foregroundColor(.gray)
+                                .padding(.leading)
+                        }
+                        TextEditor(text: $customMessage)
+                            .foregroundColor(.black)
+                            .frame(height: 50)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 10).fill(.white))
+                            .cornerRadius(10)
+                    }
+                }
+                .padding(.horizontal, 20)
+                
+                Spacer()
+                
+                Button(action: {
+                    next = (selectedTemplate != nil || !customMessage.isEmpty)
+                }) {
+                    HStack {
+                        Text("Next")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.black)
+                        
+                        Image(systemName: "arrow.right.circle.fill")
+                            .font(.title)
+                            .foregroundColor(.black)
+                    }
+                    .padding(12.5)
+                    .frame(maxWidth: .infinity)
+                    .background(.white)
+                    .cornerRadius(15)
+                    .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 10)
+                }
+                .padding(.horizontal, 20)
+                .padding(.bottom, 40)
+                .background(
+                    NavigationLink(
+                        destination: RedMessageView(),
+                        isActive: $next,
+                        label: { EmptyView() }
+                    )
+                )
+            }
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: backButton)
+            .ignoresSafeArea(.all)
+        }
+    }
+    
+    private var backButton: some View {
+        Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }) {
+            HStack {
+                Image(systemName: "chevron.left")
+                    .foregroundColor(.white)
+                Text("Back")
+                    .foregroundColor(.white)
+            }
         }
     }
 }

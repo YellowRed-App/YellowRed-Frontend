8b29cce686fcb7a0163fefc6140f280eca2d7c4c
KrishMehta <kvm3jp@virginia.edu>
2023-06-30 19:04:40 +0800

    Add duration parameter to trigger haptic feedback function and trigger continuous haptic feedback for five seconds on yellow button activation

--- YellowRed/YellowRedView.swift
+++ YellowRed/YellowRedView.swift
@@ -55,12 +55,13 @@ struct YellowRedView: View {
                                     self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
                                         if self.countdown > 0 {
                                             self.countdown -= 1
-                                            triggerHapticFeedback()
+                                            triggerHapticFeedback(0.1)
                                         } else {
                                             self.timer?.invalidate()
                                             self.timer = nil
                                             self.yellowButton.toggle()
                                             self.yellowButtonAction()
+                                            triggerHapticFeedback(5)
                                         }
                                     }
                                 } else {
@@ -171,12 +172,12 @@ struct YellowRedView: View {
         engine = nil
     }
     
-    private func triggerHapticFeedback() {
+    private func triggerHapticFeedback(_ duration: Double) {
         guard let engine = engine else { return }
         
         let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1.0)
         let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1.0)
-        let event = CHHapticEvent(eventType: .hapticContinuous, parameters: [intensity, sharpness], relativeTime: 0, duration: 0.1)
+        let event = CHHapticEvent(eventType: .hapticContinuous, parameters: [intensity, sharpness], relativeTime: 0, duration: duration)
         
         do {
             let pattern = try CHHapticPattern(events: [event], parameters: [])

df4c8c432dfbbe6442611dea344cf525d44cd091
KrishMehta <kvm3jp@virginia.edu>
2023-06-30 16:13:32 +0800

    Implement basic YellowRed main page view with ProfileView navigation functionality; pending yellow button action and red button action algorithms

--- YellowRed/YellowRedView.swift
+++ YellowRed/YellowRedView.swift
@@ -8,15 +8,77 @@
 import SwiftUI
 
 struct YellowRedView: View {
+    @State private var profile = false
+    
+    @State private var yellowButton = false
+    @State private var redButton = false
+
     var body: some View {
         ZStack {
             LinearGradient(
-                gradient: Gradient(colors: [Color.yellow, Color.red]),
+                gradient: Gradient(colors: [.blue, .white]),
                 startPoint: .topLeading,
                 endPoint: .bottomTrailing
             )
             .edgesIgnoringSafeArea(.all)
+            
+            Spacer()
+
+            VStack(spacing: 50) {
+                Spacer()
+                
+                Text("YellowRed")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                    .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
+
+                Button(action: {
+                    yellowButton.toggle()
+                    yellowButtonAction()
+                }) {
+                    Circle()
+                        .fill(.yellow)
+                        .frame(width: 200, height: 200)
+                }
+
+                Button(action: {
+                    redButton.toggle()
+                    redButtonAction()
+                }) {
+                    Circle()
+                        .fill(.red)
+                        .frame(width: 200, height: 200)
+                }
+
+                Spacer()
+
+                Button(action: {
+                    profile = true
+                }) {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .frame(width: 64, height: 64)
+                        .foregroundColor(.black)
+                        .padding(.bottom, 20)
+                }
+                .background(
+                    NavigationLink(
+                        destination: ProfileView(),
+                        isActive: $profile,
+                        label: { EmptyView() }
+                    )
+                )
+            }
         }
+    }
+
+    private func yellowButtonAction() {
+        // TODO: yellow button
+    }
+
+    private func redButtonAction() {
+        // TODO: red button
     }
 }
 

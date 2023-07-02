45569cc0d8aad5acf407f74eb9e4e0e89587e276
KrishMehta <kvm3jp@virginia.edu>
2023-07-02 23:39:44 +0800

    Standardise shadow modifier on all UI elements in get started name view

--- YellowRed/GetStartedNameView.swift
+++ YellowRed/GetStartedNameView.swift
@@ -28,13 +28,13 @@ struct GetStartedNameView: View {
                         .font(.largeTitle)
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
-                        .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
+                        .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 0)
                     
                     Text("Enter Full Name")
                         .font(.title2)
                         .fontWeight(.semibold)
                         .foregroundColor(.white)
-                        .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
+                        .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 0)
                     
                     TextField("John Smith", text: $fullName)
                         .font(.title3)
@@ -52,7 +52,6 @@ struct GetStartedNameView: View {
                         Text("Please enter a valid name!")
                             .font(.subheadline)
                             .foregroundColor(.white)
-                            .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
                     }
                     
                     Button(action: {
@@ -67,25 +66,22 @@ struct GetStartedNameView: View {
                             
                             Image(systemName: "arrow.right.circle.fill")
                                 .font(.title)
+                                .fontWeight(.semibold)
                                 .foregroundColor(.black)
                         }
                         .padding(12.5)
                         .frame(maxWidth: .infinity)
                         .background(.white)
                         .cornerRadius(15)
-                        .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 10)
+                        .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 0)
                     }
                     .padding(.horizontal, 20)
-                    .padding(.bottom, 25)
                     .background(
                         NavigationLink(
                             destination: GetStartedNumberView(fullName: fullName),
                             isActive: $next,
-                            label: {
-                                EmptyView()
-                            }
+                            label: { EmptyView() }
                         )
-                        .hidden()
                     )
                 }
                 .padding(.horizontal)

bc7c23da2f7a18f502ff5e8c3dd66967be9a47b3
KrishMehta <kvm3jp@virginia.edu>
2023-07-04 22:51:19 +0800

    Hide default back button and add custom back button to profile view

--- YellowRed/ProfileView.swift
+++ YellowRed/ProfileView.swift
@@ -74,6 +74,8 @@ struct ProfileView: View {
             }
             .background(.white)
         }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: BackButton())
     }
 }
 

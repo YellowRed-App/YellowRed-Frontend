7bb60e87bd4e50918a1c24b57176f870e590ed4a
KrishMehta <kvm3jp@virginia.edu>
2023-07-04 16:35:18 +0800

    Refactor global haptic manager observable object class into separate view file to abstract complex backend functionality

--- YellowRed/YellowRedView.swift
+++ YellowRed/YellowRedView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import CoreHaptics
 
 struct YellowRedView: View {
     @State private var profile: Bool = false
@@ -353,48 +352,5 @@ struct YellowRedView_Previews: PreviewProvider {
             .environmentObject(GlobalHapticManager.shared)
         RedButtonView(redButton: $redButton)
             .environmentObject(GlobalHapticManager.shared)
-    }
-}
-
-class GlobalHapticManager: ObservableObject {
-    static let shared = GlobalHapticManager()
-    @Published var engine: CHHapticEngine?
-    
-    private init() {
-        startHapticEngine()
-    }
-    
-    deinit {
-        stopHapticEngine()
-    }
-    
-    public func startHapticEngine() {
-        do {
-            self.engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("Failed to start haptic engine: \(error.localizedDescription)")
-        }
-    }
-    
-    public func stopHapticEngine() {
-        engine?.stop(completionHandler: nil)
-        engine = nil
-    }
-    
-    public func triggerHapticFeedback(_ duration: Double) {
-        guard let engine = engine else { return }
-        
-        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1.0)
-        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1.0)
-        let event = CHHapticEvent(eventType: .hapticContinuous, parameters: [intensity, sharpness], relativeTime: 0, duration: duration)
-        
-        do {
-            let pattern = try CHHapticPattern(events: [event], parameters: [])
-            let player = try engine.makePlayer(with: pattern)
-            try player.start(atTime: 0)
-        } catch {
-            print("Failed to play haptic feedback: \(error.localizedDescription)")
-        }
     }
 }

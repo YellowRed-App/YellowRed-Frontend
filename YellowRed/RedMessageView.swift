92ec517897eb6a2fddaafa4fc67af15ac4ccfdf7
KrishMehta <kvm3jp@virginia.edu>
2023-07-10 16:47:25 +0800

    Prevent user from editing red message template by long pressing text field by using text element instead; hide description text and next button when user is selecting a selecting template; add dispatch queue to asynchronously hide error message after five seconds; adjust padding in red message view

--- YellowRed/RedMessageView.swift
+++ YellowRed/RedMessageView.swift
@@ -19,6 +19,7 @@ struct RedMessageView: View {
     @State private var redMessage: String = ""
     
     @State private var valid: Bool = true
+    @State private var error: Bool = false
     
     @State private var next: Bool = false
     
@@ -55,7 +56,7 @@ struct RedMessageView: View {
                     .foregroundColor(.white)
                     .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 0)
                 
-                if selectingTemplate == nil {
+                if !isSelecting && selectingTemplate == nil {
                     Text("Please choose a message template. There is no custom message option for the Red Button!")
                         .font(.title3)
                         .fontWeight(.medium)
@@ -95,7 +96,7 @@ struct RedMessageView: View {
                             .background(.white)
                             .cornerRadius(10)
                             .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 0)
-                            .padding(.horizontal, 10)
+                            .padding(.horizontal, 20)
                             
                             Button("Cancel", action: {
                                 selectingTemplate = nil
@@ -108,43 +109,44 @@ struct RedMessageView: View {
                             .background(.white)
                             .cornerRadius(10)
                             .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 0)
-                            .padding(.horizontal, 10)
+                            .padding(.horizontal, 20)
                         }
+                        .padding(.vertical, 20)
                     } else {
                         ForEach(0..<messageTemplates.count, id: \.self) { index in
-                            TextField("Placeholder", text: Binding(
-                                get: { self.messageTemplates[index] },
-                                set: { newValue in
-                                    self.messageTemplates[index] = newValue
-                                    self.selectedTemplate = nil
+                            Text(self.messageTemplates[index])
+                                .font(.body)
+                                .fontWeight(.regular)
+                                .foregroundColor(.black)
+                                .lineLimit(1)
+                                .padding(12.5)
+                                .frame(maxWidth: .infinity)
+                                .background(selectedTemplate == index ? .white.opacity(0.5) : .white)
+                                .cornerRadius(15)
+                                .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 0)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(selectedTemplate == index ? .black : .clear, lineWidth: 2.5)
+                                )
+                                .onTapGesture {
+                                    self.selectingTemplate = index
+                                    self.isSelecting = true
                                 }
-                            ))
-                            .font(.body)
-                            .fontWeight(.regular)
-                            .foregroundColor(.black)
-                            .padding(12.5)
-                            .frame(maxWidth: .infinity)
-                            .background(selectedTemplate == index ? .white.opacity(0.5) : .white)
-                            .cornerRadius(15)
-                            .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 0)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(selectedTemplate == index ? .black : .clear, lineWidth: 2.5)
-                            )
-                            .onTapGesture {
-                                self.selectingTemplate = index
-                                self.isSelecting = true
-                            }
                         }
                     }
                     
                     if !isSelecting && selectingTemplate == nil {
-                        if !valid {
+                        if error {
                             Text("Please choose a template!")
                                 .font(.subheadline)
                                 .foregroundColor(.white)
                                 .multilineTextAlignment(.center)
                                 .fixedSize(horizontal: false, vertical: true)
+                                .onAppear {
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                        error = false
+                                    }
+                                }
                         }
                     }
                 }
@@ -152,34 +154,38 @@ struct RedMessageView: View {
                 
                 Spacer()
                 
-                Button(action: {
-                    valid = selectedTemplate != nil
-                    if valid {
-                        redMessage = messageTemplates[selectedTemplate!]
-                        next = true
+                if !isSelecting && selectingTemplate == nil {
+                    Button(action: {
+                        valid = selectedTemplate != nil
+                        if valid {
+                            redMessage = messageTemplates[selectedTemplate!]
+                            next = true
+                        } else {
+                            error = true
+                        }
+                    }) {
+                        HStack {
+                            Text("Next")
+                            Image(systemName: "arrow.right.circle.fill")
+                        }
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.black)
+                        .padding(12.5)
+                        .frame(maxWidth: .infinity)
+                        .background(.white)
+                        .cornerRadius(15)
+                        .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 0)
                     }
-                }) {
-                    HStack {
-                        Text("Next")
-                        Image(systemName: "arrow.right.circle.fill")
-                    }
-                    .font(.title)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.black)
-                    .padding(12.5)
-                    .frame(maxWidth: .infinity)
-                    .background(.white)
-                    .cornerRadius(15)
-                    .shadow(color: .black.opacity(0.5), radius: 10, x: 0, y: 0)
-                }
-                .background(
-                    NavigationLink(
-                        destination: YellowRedView(),
-                        isActive: $next,
-                        label: { EmptyView() }
+                    .background(
+                        NavigationLink(
+                            destination: YellowRedView(),
+                            isActive: $next,
+                            label: { EmptyView() }
+                        )
                     )
-                )
-                .padding(.horizontal, 20)
+                    .padding(.horizontal, 20)
+                }
             }
             .padding(.horizontal, 20)
             .padding(.bottom, 40)

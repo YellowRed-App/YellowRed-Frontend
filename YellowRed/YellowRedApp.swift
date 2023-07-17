109b23b402afd14f0aa05bf58c5870157827f8c7
KrishMehta <kvm3jp@virginia.edu>
2023-07-17 16:49:35 +0800

    Add FirebaseCore initialisation code to main entry point in order to connect Firebase on app start up

--- YellowRed/YellowRedApp.swift
+++ YellowRed/YellowRedApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct YellowRedApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     @State var fullName: String = "John Smith"
     @State var phoneNumber: String = "(123) 456-7890"
     @State var emailAddress: String = "abc5xy@virginia.edu"

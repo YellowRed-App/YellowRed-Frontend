127f3163935214d7c6af32c6a6dc83a263d2da9c
Krish Mehta <me@krishvmehta.com>
2023-08-30 18:46:36 -0400

    Remove redundant state variables in app main entry point

--- YellowRed/YellowRedApp.swift
+++ YellowRed/YellowRedApp.swift
@@ -20,18 +20,6 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 struct YellowRedApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
-    @State var fullName: String = "John Smith"
-    @State var phoneNumber: String = "(123) 456-7890"
-    @State var emailAddress: String = "abc5xy@virginia.edu"
-    @State var affiliation: String = "Other"
-    @State var university: String = "University of Michigan"
-    @State var emergencyContacts: [EmergencyContact] = [
-        EmergencyContact(isSelected: true, displayName: "John Doe", phoneNumber: "+1 (234) 567-8901"),
-        EmergencyContact(isSelected: true, displayName: "Jane Doe", phoneNumber: "+1 (234) 567-8902"),
-        EmergencyContact(isSelected: true, displayName: "Baby Doe", phoneNumber: "+1 (234) 567-8903")
-    ]
-    @State var yellowMessage: String = "I'm feeling a bit uncomfortable, can we talk"
-    
     var body: some Scene {
         WindowGroup {
             HomeScreenView()

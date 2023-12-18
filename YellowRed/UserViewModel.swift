60644695b1c9604f9a44748ba9a66a2fc77a32a5
Krish Mehta <me@krishvmehta.com>
2023-12-18 16:35:36 +0800

    Implement `updateUser` wrapper method in UserViewModel class

--- YellowRed/UserViewModel.swift
+++ YellowRed/UserViewModel.swift
@@ -10,6 +10,13 @@ import Foundation
 class UserViewModel: ObservableObject {
     @Published var userId: String?
     
+    @Published var fullName: String = ""
+    @Published var phoneNumber: String = ""
+    @Published var emailAddress: String = ""
+    @Published var emergencyContacts: [EmergencyContact] = []
+    @Published var yellowMessage: String = ""
+    @Published var redMessage: String = ""
+    
     private let firestoreManager = FirestoreManager()
     
     func createUser(userUID: String, fullName: String, phoneNumber: String, emailAddress: String, affiliation: String, university: String, completion: @escaping (Bool) -> Void) {
@@ -19,6 +26,21 @@ class UserViewModel: ObservableObject {
                 completion(false)
             } else {
                 self.userId = userUID
+                completion(true)
+            }
+        }
+    }
+    
+    func updateUser(userId: String, phoneNumber: String, emailAddress: String, completion: @escaping (Bool) -> Void) {
+        firestoreManager.updateUser(userId: userId, phoneNumber: phoneNumber, emailAddress: emailAddress) { error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(false)
+            } else {
+                DispatchQueue.main.async {
+                    self.phoneNumber = phoneNumber
+                    self.emailAddress = emailAddress
+                }
                 completion(true)
             }
         }

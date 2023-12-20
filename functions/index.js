3b2750dec611d718fcdf4d62618f0845aea3ec7c
Krish Mehta <me@krishvmehta.com>
2023-12-20 09:20:38 +0800

    Add environment-specific Twilio configuration and debugging logs
    The Twilio configuration now depends on the environment: production uses Firebase config, others use environment variables. Debugging message logs have been added to the sendEmergencySMS function, giving more insight into the request payload and the success or error responses.

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,34 @@
 const functions = require('firebase-functions');
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const twilio = require('twilio');
 
-// Twilio configuration from Firebase config
-const twilioAccountSid = functions.config().twilio.sid;
-const twilioAuthToken = functions.config().twilio.token;
-const twilioPhoneNumber = functions.config().twilio.phone;
+const twilioAccountSid = process.env.NODE_ENV === 'production'
+    ? functions.config().twilio.sid
+    : process.env.TWILIO_ACCOUNT_SID;
+const twilioAuthToken = process.env.NODE_ENV === 'production'
+    ? functions.config().twilio.token
+    : process.env.TWILIO_AUTH_TOKEN;
+const twilioPhoneNumber = process.env.NODE_ENV === 'production'
+    ? functions.config().twilio.phone
+    : process.env.TWILIO_PHONE_NUMBER;
 
 const client = new twilio(twilioAccountSid, twilioAuthToken);
 
 exports.sendEmergencySMS = functions.https.onRequest((req, res) => {
+    console.log("Function triggered with body:", req.body);
+
     const { contacts, message } = req.body;
 
     if (!contacts || !message) {
+        console.log("Missing contacts or message");
         return res.status(400).send('Contacts and message are required');
     }
+
+    console.log("Sending messages to contacts:", contacts);
 
     const smsPromises = contacts.map(contact => {
         return client.messages.create({
@@ -24,6 +39,12 @@ exports.sendEmergencySMS = functions.https.onRequest((req, res) => {
     });
 
     Promise.all(smsPromises)
-        .then(results => res.status(200).json({ success: true, results }))
-        .catch(error => res.status(500).json({ success: false, error: error.message }));
+        .then(results => {
+            console.log("Messages sent:", results);
+            res.status(200).json({ success: true, results })
+        })
+        .catch(error => {
+            console.error("Error sending messages:", error);
+            res.status(500).json({ success: false, error: error.message });
+        });
 });

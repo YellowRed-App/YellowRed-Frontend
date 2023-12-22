309d2631666acbccb43981ecc563f3104f135f47
Krish Mehta <me@krishvmehta.com>
2023-12-22 16:30:44 +0800

    Refactor the shit out of FirestoreManager class for better consistency

--- YellowRed/FirestoreManager.swift
+++ YellowRed/FirestoreManager.swift
@@ -12,128 +12,102 @@ class FirestoreManager {
     
     private let db = Firestore.firestore()
     
-    func createUser(userUID: String, fullName: String, phoneNumber: String, emailAddress: String, affiliation: String, university: String, completion: @escaping (Error?) -> Void) {
-        let userRef = db.collection("users")
-        let userData: [String: Any] = [
+    func createUser(userId: String, fullName: String, phoneNumber: String, emailAddress: String, affiliation: String, university: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        let userData = [
             "fullName": fullName,
             "phoneNumber": phoneNumber,
             "emailAddress": emailAddress,
             "affiliation": affiliation,
             "university": university
         ]
-        
-        userRef.document(userUID).setData(userData) { error in
-            completion(error)
+        db.collection("users").document(userId).setData(userData) { error in
+            self.handleCompletion(error, completion: completion)
         }
     }
     
-    func updateUser(userId: String, phoneNumber: String, emailAddress: String, completion: @escaping (Error?) -> Void) {
-        let userRef = db.collection("users").document(userId)
-        userRef.updateData([
+    func updateUser(userId: String, phoneNumber: String, emailAddress: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        let updateData = [
             "phoneNumber": phoneNumber,
             "emailAddress": emailAddress
-        ]) { error in
-            if let error = error {
-                print("Error updating user: \(error.localizedDescription)")
-                completion(error)
-            } else {
-                completion(nil)
-            }
+        ]
+        db.collection("users").document(userId).updateData(updateData) { error in
+            self.handleCompletion(error, completion: completion)
         }
     }
     
-    func addEmergencyContactsToUser(userId: String, emergencyContacts: [EmergencyContact], completion: @escaping (Error?) -> Void) {
-        let userRef = db.collection("users").document(userId)
-        
-        let batch = db.batch()
-        
-        for contact in emergencyContacts {
-            let contactRef = userRef.collection("emergencyContacts").document()
-            let contactData: [String: Any] = [
-                "displayName": contact.displayName,
-                "phoneNumber": contact.phoneNumber
-            ]
-            batch.setData(contactData, forDocument: contactRef)
-        }
-        
-        batch.commit { error in
-            completion(error)
-        }
-    }
-    
-    func updateEmergencyContactsForUser(userId: String, emergencyContacts: [EmergencyContact], completion: @escaping (Error?) -> Void) {
-        let userRef = db.collection("users").document(userId)
-        let contactsRef = userRef.collection("emergencyContacts")
-        
-        contactsRef.getDocuments { (snapshot, error) in
+    func updateEmergencyContacts(userId: String, emergencyContacts: [EmergencyContact], completion: @escaping (Result<Void, Error>) -> Void) {
+        let contactsRef = db.collection("users").document(userId).collection("emergencyContacts")
+        contactsRef.getDocuments { snapshot, error in
             if let error = error {
-                completion(error)
+                print("Error")
+                completion(.failure(error))
                 return
             }
-            
-            self.db.runTransaction({ (transaction, errorPointer) -> Any? in
-                // delete existing contacts
-                snapshot?.documents.forEach { document in
-                    transaction.deleteDocument(document.reference)
-                }
 
-                // add new contacts
-                emergencyContacts.forEach { contact in
-                    let newContactRef = contactsRef.document()
-                    transaction.setData([
-                        "displayName": contact.displayName,
-                        "phoneNumber": contact.phoneNumber
-                    ], forDocument: newContactRef)
-                }
-                
-                return nil
-            }) { (_, error) in
-                completion(error)
+            let batch = self.db.batch()
+            snapshot?.documents.forEach { document in
+                batch.deleteDocument(document.reference)
+            }
+
+            emergencyContacts.forEach { contact in
+                let newContactRef = contactsRef.document()
+                let contactData = [
+                    "displayName": contact.displayName,
+                    "phoneNumber": contact.phoneNumber
+                ]
+                batch.setData(contactData, forDocument: newContactRef)
+            }
+
+            batch.commit { error in
+                self.handleCompletion(error, completion: completion)
             }
         }
     }
     
-    func addYellowRedMessagesToUser(userId: String, yellowMessage: String, redMessage: String, completion: @escaping (Error?) -> Void) {
-        let userRef = db.collection("users").document(userId)
-        userRef.updateData([
+    func updateYellowRedMessages(userId: String, yellowMessage: String, redMessage: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        let updateData = [
             "yellowMessage": yellowMessage,
             "redMessage": redMessage
-        ]) { error in
-            completion(error)
-        }
-    }
-    
-    func updateYellowRedMessagesForUser(userId: String, yellowMessage: String, redMessage: String, completion: @escaping (Error?) -> Void) {
-        let userRef = db.collection("users").document(userId)
-        userRef.updateData([
-            "yellowMessage": yellowMessage,
-            "redMessage": redMessage
-        ]) { error in
-            completion(error)
+        ]
+        db.collection("users").document(userId).updateData(updateData) { error in
+            self.handleCompletion(error, completion: completion)
         }
     }
     
     func fetchUserData(userId: String, completion: @escaping (Result<[String: Any], Error>) -> Void) {
-        let userRef = db.collection("users").document(userId)
-        userRef.getDocument { document, error in
+        db.collection("users").document(userId).getDocument { document, error in
             if let error = error {
                 completion(.failure(error))
             } else if let document = document, document.exists {
                 completion(.success(document.data() ?? [:]))
             } else {
-                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Document does not exist"])))
+                self.handleNonExistentDocument(completion: completion)
             }
         }
     }
     
-    func fetchEmergencyContactsForUser(userId: String, completion: @escaping (Result<[EmergencyContact], Error>) -> Void) {
-        let contactsRef = db.collection("users").document(userId).collection("emergencyContacts")
-        contactsRef.getDocuments { querySnapshot, error in
+    func fetchYellowRedMessages(userId: String, completion: @escaping (Result<(yellowMessage: String, redMessage: String), Error>) -> Void) {
+        db.collection("users").document(userId).getDocument { document, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let document = document, document.exists {
+                let data = document.data() ?? [:]
+                let yellowMessage = data["yellowMessage"] as? String ?? ""
+                let redMessage = data["redMessage"] as? String ?? ""
+                completion(.success((yellowMessage: yellowMessage, redMessage: redMessage)))
+            } else {
+                self.handleNonExistentDocument(completion: completion)
+            }
+        }
+    }
+    
+    func fetchEmergencyContacts(userId: String, completion: @escaping (Result<[EmergencyContact], Error>) -> Void) {
+        db.collection("users").document(userId).collection("emergencyContacts").getDocuments { snapshot, error in
             if let error = error {
                 completion(.failure(error))
             } else {
                 var contacts: [EmergencyContact] = []
-                querySnapshot?.documents.forEach { document in
+                snapshot?.documents.forEach { document in
                     let data = document.data()
                     let contact = EmergencyContact(
                         isSelected: false,
@@ -147,20 +121,18 @@ class FirestoreManager {
         }
     }
     
-    func fetchYellowRedMessagesForUser(userId: String, completion: @escaping (Result<(yellowMessage: String, redMessage: String), Error>) -> Void) {
-        let userRef = db.collection("users").document(userId)
-        userRef.getDocument { document, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let document = document, document.exists {
-                let data = document.data() ?? [:]
-                let yellowMessage = data["yellowMessage"] as? String ?? ""
-                let redMessage = data["redMessage"] as? String ?? ""
-                completion(.success((yellowMessage: yellowMessage, redMessage: redMessage)))
-            } else {
-                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Document does not exist"])))
-            }
+    private func handleCompletion<T>(_ error: Error?, completion: @escaping (Result<T, Error>) -> Void) {
+        if let error = error {
+            NSLog("Error: \(error.localizedDescription)")
+            completion(.failure(error))
+        } else {
+            completion(.success(() as! T))
         }
+    }
+    
+    private func handleNonExistentDocument<T>(completion: (Result<T, Error>) -> Void) {
+        let error = NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Document does not exist"])
+        completion(.failure(error))
     }
     
 }

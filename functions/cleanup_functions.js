8e2a5917c8af3295b0a9dbcc29b701dbf60634b9
Krish Mehta <me@krishvmehta.com>
2023-12-24 11:32:52 +0800

    Update cleanup functions routine to delete old sessions
    The daily cleanup function has been updated to delete old Red Button sessions instead of location updates. The function now targets the 'sessions' collection, and removes sessions that were deactivated more than seven days ago. All respective references in other parts of the application have been updated accordingly.

--- functions/cleanup_functions.js
+++ functions/cleanup_functions.js
@@ -5,7 +5,7 @@ if (admin.apps.length === 0) {
     admin.initializeApp();
 }
 
-exports.deleteLocationUpdatesForRedButton = functions.pubsub.schedule('every 24 hours').onRun(() => {
+exports.deleteOldRedButtonSessions = functions.pubsub.schedule('every 24 hours').onRun(() => {
     console.log("Scheduled cleanup function triggered");
 
     const db = admin.firestore();
@@ -13,29 +13,29 @@ exports.deleteLocationUpdatesForRedButton = functions.pubsub.schedule('every 24 
     const now = new Date();
     const sevenDaysAgo = new Date(now.getTime() - (7 * 24 * 60 * 60 * 1000));
 
-    return db.collectionGroup('locationUpdates')
+    return db.collectionGroup('sessions')
         .where('deactivationTime', '<=', sevenDaysAgo)
         .get()
         .then(snapshot => {
-            console.log(`Found ${snapshot.size} location updates to delete`);
+            console.log(`Found ${snapshot.size} sessions to delete`);
             let batch = db.batch();
 
             if (snapshot.empty) {
-                console.log('No location updates found to delete.');
+                console.log('No sessions found to delete.');
                 return null;
             }
 
             snapshot.docs.forEach(doc => {
-                console.log(`Deleting document with ID: ${doc.id}`);
+                console.log(`Deleting session with ID: ${doc.id}`);
                 batch.delete(doc.ref);
             });
 
             return batch.commit().then(() => {
-                console.log('Successfully deleted location updates.');
+                console.log('Successfully deleted old sessions.');
             });
         })
         .catch(error => {
-            console.error('Error deleting location updates:', error);
+            console.error('Error deleting sessions:', error);
             throw new Error('Error in Firestore operation');
         });
 });
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const {sendEmergencySMS} = require('./twilio_functions')
-const {deleteLocationUpdatesForRedButton} = require('./cleanup_functions');
+const {deleteOldRedButtonSessions} = require('./cleanup_functions');
 
 exports.sendEmergencySMS = sendEmergencySMS;
-exports.deleteLocationUpdatesForRedButton = deleteLocationUpdatesForRedButton;
+exports.deleteOldRedButtonSessions = deleteOldRedButtonSessions;

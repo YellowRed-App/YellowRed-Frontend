0cbdc2c84ced1a150ec07060cf9b0a38564b5eca
Krish Mehta <me@krishvmehta.com>
2023-12-24 16:08:14 +0800

    Add location functions to exports
    The location-related functions, `fetchActiveSessions` and `fetchLocationUpdates`, have been included in the exports in the `index.js` file. These additions will make these functions accessible for use elsewhere in the codebase.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
-const {sendEmergencySMS} = require('./twilio_functions')
+const {sendEmergencySMS} = require('./twilio_functions');
 const {deleteOldRedButtonSessions} = require('./cleanup_functions');
+const {fetchActiveSessions, fetchLocationUpdates} = require('./location_functions');
 
 exports.sendEmergencySMS = sendEmergencySMS;
 exports.deleteOldRedButtonSessions = deleteOldRedButtonSessions;
+exports.fetchActiveSessions = fetchActiveSessions;
+exports.fetchLocationUpdates = fetchLocationUpdates;

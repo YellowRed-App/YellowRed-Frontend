aac72f9a0b556df12945e4d4677e6f3f52f2543a
Krish Mehta <me@krishvmehta.com>
2023-12-24 16:07:55 +0800

    Add function to fetch location updates for active sessions
    A new function has been added to the location functions file to fetch location updates in sessions for a specific user. This function requires the user ID and session ID as input parameters and it retrieves related location updates from the Firestore database.

--- functions/location_functions.js
+++ functions/location_functions.js
@@ -35,3 +35,30 @@ exports.fetchActiveSessions = functions.https.onRequest(async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
+
+// function to get location updates for a session
+exports.fetchLocationUpdates = functions.https.onRequest(async (req, res) => {
+    console.log("Function triggered to get location updates.");
+
+    const {userId, sessionId} = req.query;
+    if (!userId || !sessionId) {
+        console.error("Missing user ID or session ID");
+        return res.status(400).send('User ID and Session ID are required');
+    }
+
+    try {
+        const locationUpdatesSnapshot = await admin.firestore()
+            .collection('users').doc(userId)
+            .collection('sessions').doc(sessionId)
+            .collection('locationUpdates')
+            .get();
+
+        const locationUpdates = locationUpdatesSnapshot.docs.map(doc => doc.data());
+
+        console.log("Location updates:", locationUpdates);
+        res.status(200).json(locationUpdates);
+    } catch (error) {
+        console.error('Error fetching location updates:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});

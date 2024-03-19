5224e2df9ff8e47682b617600acebbeaee1244b2
Krish <me@krishvmehta.com>
2024-03-19 08:21:09 -0400

    Implement `validateUserDataComplete` function to check if set up process is complete

--- YellowRed/UserAuth.swift
+++ YellowRed/UserAuth.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestore
 
 class UserAuth: ObservableObject {
     @Published var isUserAuthenticated: Bool = false
+    @Published var isUserDataComplete: Bool = false
+    
+    private var firestoreManager = FirestoreManager()
     
     init() {
         updateAuthenticationStatus()
@@ -19,9 +23,47 @@ class UserAuth: ObservableObject {
         if let user = Auth.auth().currentUser {
             print("User is logged in: \(user.uid)")
             isUserAuthenticated = true
+            validateUserDataComplete(userID: user.uid)
         } else {
             print("No user is logged in.")
             isUserAuthenticated = false
+            isUserDataComplete = false
+        }
+    }
+    
+    func validateUserDataComplete(userID: String) {
+        firestoreManager.fetchUserData(userId: userID) { [weak self] result in
+            switch result {
+            case .success(let data):
+                let emailAddress = data["emailAddress"] as? String ?? ""
+                self?.firestoreManager.fetchEmergencyContacts(userId: userID) { result in
+                    switch result {
+                    case .success(let contacts):
+                        let hasContacts = !contacts.isEmpty
+                        self?.firestoreManager.fetchYellowRedMessages(userId: userID) { result in
+                            switch result {
+                            case .success(let messages):
+                                let hasMessages = !messages.yellowMessage.isEmpty && !messages.redMessage.isEmpty
+                                DispatchQueue.main.async {
+                                    self?.isUserDataComplete = !emailAddress.isEmpty && hasContacts && hasMessages
+                                }
+                            case .failure:
+                                DispatchQueue.main.async {
+                                    self?.isUserDataComplete = false
+                                }
+                            }
+                        }
+                    case .failure:
+                        DispatchQueue.main.async {
+                            self?.isUserDataComplete = false
+                        }
+                    }
+                }
+            case .failure:
+                DispatchQueue.main.async {
+                    self?.isUserDataComplete = false
+                }
+            }
         }
     }
 }

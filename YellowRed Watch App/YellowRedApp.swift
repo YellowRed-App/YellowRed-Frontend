5c18abfad35e54e12cc5b959e45fef50d3b452bf
Krish Mehta <me@krishvmehta.com>
2024-06-09 09:51:23 -0400

    Add watchOS app target for existing iOS app

---
+++ YellowRed Watch App/YellowRedApp.swift
@@ -0,0 +1,17 @@
+//
+//  YellowRedApp.swift
+//  YellowRed Watch App
+//
+//  Created by Krish Mehta on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct YellowRed_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
